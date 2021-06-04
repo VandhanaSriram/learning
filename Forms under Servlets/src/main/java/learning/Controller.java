126bd1073963dcb1e9699b64566ae18f27990584
Vandhana Vaishnavi <Vandhana Vaishnavi@192.168.0.18>
2021-06-04 13:49:58 -0400

    Form Implementation and submission into a servlet

---
+++ Forms under Servlets/src/main/java/learning/Controller.java
@@ -0,0 +1,69 @@
+package learning;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().print("Form data handled in doGet() method" +"<br>");
+		response.getWriter().print("Name: " +request.getParameter("name") +"<br>");
+		response.getWriter().print("Gender: " +request.getParameter("gender") +"<br>");
+		String[] languages = request.getParameterValues("lang");
+		if(languages !=null) {
+			response.getWriter().print("Languages:");
+			for(int i=0; i<languages.length; i++) {
+				response.getWriter().print("<br>");
+				response.getWriter().print(languages[i]);
+			}
+		}
+		else {
+			response.getWriter().print("No languages selected" +"<br>");
+		}
+		response.getWriter().print("<br>");
+		response.getWriter().print("Country: " +request.getParameter("country"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().print("Form data handled in doPost() method" +"<br>");
+		response.getWriter().print("Name: " +request.getParameter("name") +"<br>");
+		response.getWriter().print("Gender: " +request.getParameter("gender") +"<br>");
+		String[] languages = request.getParameterValues("lang");
+		if(languages !=null) {
+			response.getWriter().print("Languages:");
+			for(int i=0; i<languages.length; i++) {
+				response.getWriter().print("<br>");
+				response.getWriter().print(languages[i]);
+			}
+		}
+		else {
+			response.getWriter().print("No languages selected" +"<br>");
+		}
+		response.getWriter().print("<br>");
+		response.getWriter().print("Country: " +request.getParameter("country"));
+	}
+
+}
---
+++ Forms under Servlets/src/main/webapp/form.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Form demo</title>
+</head>
+<body>
+<form action="<%= request.getContextPath() %>/Controller" method="post">
+	Full name: <input type="text" name="name"><br>
+	Gender: <input type="radio" name="gender" value="male">Male
+			<input type="radio" name="gender" value="female">Female<br>
+	Languages known: <input type="checkbox" name="lang" value="english">English
+					 <input type="checkbox" name="lang" value="hindi">Hindi
+					 <input type="checkbox" name="lang" value="french">French <br>
+	Country:
+	<select name="country">
+		<option value="india">India</option>
+		<option value="usa">United States of America</option>
+		<option value="china">China</option>	
+	</select><br>
+
+	<input type="submit" value="submit">
+</form>
+</body>
+</html>
